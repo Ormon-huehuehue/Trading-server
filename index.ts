4ac5cb3027b665181c961a0d9ce5258932b2d837
ormon <armnvrma10@gmail.com>
2025-07-31 22:04:38 +0530

    setting up endpoints

--- index.ts
+++ index.ts
@@ -42,3 +42,70 @@ const users :  User[] = [{
 
 const bids : Order[] = [];
 const asks : Order[] = [];
+
+
+//place a limit order 
+app.post("/order", (req:  any, res : any)=>{
+  const side : string = req.body.side; //bid or ask
+  const price : number = req.body.price;
+  const quantity : number = req.body.quantity;
+  const userId : string = req.body.userId;
+  
+  const remainingQty = fillOrders(side, price, quantity, userId);
+
+  if(remainingQty == 0){
+    res.json({ filledQuantity : quantity});
+    return;
+  }
+
+  if(side == 'bid'){
+    bids.push({
+      userId, 
+      price, 
+      quantity : remainingQty
+    });
+    bids.sort((a,b) => a.price < b.price ? 1 : -1);
+  };
+
+  res.json({
+    filledQuantity : quantity - remainingQty
+  })
+})
+
+interface Depth{
+  [price : string] : {
+    type : "bid" | "ask",
+    quantity : number
+  }
+}
+
+
+app.get('/depth', (res:any, req:any)=>{
+  const depth : Depth = {};
+
+  for(let i = 0 ; i < bids.length ; i++){
+    if(!depth[bids[i].price]){
+      depth[bids[i].price] = {
+        quantity : bids[i].quantity,
+        type :"bid"
+      }
+    }
+    else{
+      depth[bids[i].price].quantity += bids[i].quantity;
+    }
+  };
+
+  for(let i = 0 ; i < asks.length ; i++){
+    if(!depth[asks[i].price]){
+      depth[asks[i].price] = {
+        quantity : asks[i].quantity,
+        type :"ask"
+      }
+    }
+    else{
+      depth[asks[i].price].quantity += asks[i].quantity;
+    }
+  };
+
+
+})
