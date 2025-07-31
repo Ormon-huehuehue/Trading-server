5a4e2f9d25992049da646db7304a6483578c8dab
ormon <armnvrma10@gmail.com>
2025-07-31 22:24:03 +0530

    fillorders function

--- index.ts
+++ index.ts
@@ -125,3 +125,46 @@ app.get("/balance/:userId", (req:  any, res : any)=>{
   }
   res.json({balances : user.balances});
 })
+
+
+app.get("/quote", (req : any, res:any)=>{
+  //todo
+})
+
+
+const fillOrders = (side : string, price : number, quantity : number, userId : string)=>{
+  let remainingQuantity = quantity;
+  if (side === "bid") {
+    for (let i = asks.length - 1; i >= 0; i--) {
+      if (asks[i].price > price) {
+        continue;
+      }
+      if (asks[i].quantity > remainingQuantity) {
+        asks[i].quantity -= remainingQuantity;
+        flipBalance(asks[i].userId, userId, remainingQuantity, asks[i].price);
+        return 0;
+      } else {
+        remainingQuantity -= asks[i].quantity;
+        flipBalance(asks[i].userId, userId, asks[i].quantity, asks[i].price);
+        asks.pop();
+      }
+    }
+  } else {
+    for (let i = bids.length - 1; i >= 0; i--) {
+      if (bids[i].price < price) {
+        continue;
+      }
+      if (bids[i].quantity > remainingQuantity) {
+        bids[i].quantity -= remainingQuantity;
+        flipBalance(userId, bids[i].userId, remainingQuantity, price);
+        return 0;
+      } else {
+        remainingQuantity -= bids[i].quantity;
+        flipBalance(userId, bids[i].userId, bids[i].quantity, price);
+        bids.pop();
+      }
+    }
+  }
+
+  return remainingQuantity;
+}
